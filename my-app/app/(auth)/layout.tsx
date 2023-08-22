17c4f2812496191c7d9231c7c6a6b4471752a8b4
Yodahe <ketemayodahe@gmail.com>
2023-08-22 19:51:00 +0300

    routes of the sign up and sign in pages

---
+++ my-app/app/(auth)/layout.tsx
@@ -0,0 +1,5 @@
+function AuthLAyout({ children }: { children: React.ReactNode }) {
+  return <div className="flex justify-center items-center h-full">{children}</div>;
+}
+
+export default AuthLAyout;
