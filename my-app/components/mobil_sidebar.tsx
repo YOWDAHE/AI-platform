b88b099c8ef184d5b61f3a4b682d5ddc57af2095
Yodahe <ketemayodahe@gmail.com>
2023-08-28 12:16:15 +0300

    Sidebar sheet look complete

--- my-app/components/mobil_sidebar.tsx
+++ my-app/components/mobil_sidebar.tsx
@@ -1,11 +1,32 @@
+"use client";
+
 import { Menu } from "lucide-react";
 import { Button } from "./ui/button";
+import { Sheet, SheetContent, SheetTrigger } from "./ui/sheet";
+import Sidebar from "./sidebar";
+import { useEffect, useState } from "react";
 
 function Mobile_sidebar() {
+  const [isMounted, setIsMounted] = useState(false);
+  useEffect(() => {
+    setIsMounted(true);
+  }, []);
+
+  if (!isMounted) {
+    return null;  
+  }
+
   return (
-    <Button size="sm" variant="ghost" className="md:hidden">
-      <Menu />
-    </Button>
+    <Sheet>
+      <SheetTrigger>
+        <Button size="sm" variant="ghost" className="md:hidden">
+          <Menu />
+        </Button>
+      </SheetTrigger>
+      <SheetContent side={'left'} className="bg-gray-900">
+        <Sidebar/>
+      </SheetContent>
+    </Sheet>
   );
 }
 
